5f72569d3dd0c837c67fe4d914f9a43cb3b935ee
batformat <bat.format@gmail.com>
2017-04-14 23:38:09 +0300

    fix font

--- img.php
+++ img.php
@@ -1,18 +1,21 @@
-<?php// наше изображение
-$img = ImageCreateFromJPEG("pic.jpg");
+<?php
+
+header('Content-type: image/jpg');
+
+$img = ImageCreateFromJPEG("./public/pic.jpg");
 
 // определяем цвет, в RGB
 $color = imagecolorallocate($img, 255, 0, 0);
 
 // указываем путь к шрифту
-$font = 'arial.ttf';
+$font = './public/roboto.ttf';
 
 $text = urldecode($_GET['name']);
+
 imagettftext($img, 24, 0, 365, 159, $color, $font, $text);
 // 24 - размер шрифта
 // 0 - угол поворота
 // 365 - смещение по горизонтали
 // 159 - смещение по вертикали
 
-header('Content-type: image/jpeg');
-imagejpeg($img, NULL, 100);?>
+imagejpeg($img, NULL, 100);
